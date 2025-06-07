2cd76f6c2c4df537a9b5158a4df9807f45afe05c
William L. Anderson <band@users.noreply.github.com>
2025-06-07 13:18:33 -0500

    Update main.ts
    make change described in obsidian-releases/pull/6296 regarding setting headings on settings page.

--- main.ts
+++ main.ts
@@ -391,8 +391,6 @@ class FolderListfileSettingTab extends PluginSettingTab {
     const { containerEl } = this;
     containerEl.empty();
 
-    containerEl.createEl("h2", { text: "Folder Listfile Settings" });
-
     new Setting(containerEl)
       .setName("Listfile pattern")
       .setDesc(
